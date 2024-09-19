72b4ca3f067ebd310abd968e6323352c663ead0b
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2024-09-19 18:37:54 -0500

    initial

--- app/src/main/java/com/example/firstlogin/AccountEntity.java
+++ app/src/main/java/com/example/firstlogin/AccountEntity.java
@@ -1,5 +1,10 @@
 package com.example.firstlogin;
 
 public class AccountEntity {
-
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String username;
+    private String password;
 }
