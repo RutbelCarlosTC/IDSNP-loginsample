f2fbe71c3b706631638190d468c5f6ed372d246a
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2024-09-22 12:42:28 -0500

    save data in cuentas.txt

--- app/src/main/java/com/example/firstlogin/LoginActivity.java
+++ app/src/main/java/com/example/firstlogin/LoginActivity.java
@@ -17,11 +17,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.firstlogin.databinding.ActivityMainBinding;
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class LoginActivity extends AppCompatActivity {
     private static String TAG = "MainActivity";
     private ActivityMainBinding binding;
     private AccountEntity accountEntity;
     private String accountEntityString;
+    private File accountsFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +43,24 @@ public class LoginActivity extends AppCompatActivity {
         Button btnLogin = binding.btnLogin;
         Button btnAddAccount = binding.btnAddAccount;
 
+        accountsFile = new File(getFilesDir(),"cuentas.txt");
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(edtUsername.getText().toString().equals("admin") && edtPassword.getText().toString().equals("admin")){
-                    Toast.makeText(getApplicationContext(),"Bienvenido a mi App",Toast.LENGTH_SHORT).show();
-                    Log.d(TAG,"Bienvenido a mi App");
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                if(checkCredentials (username, password)){
+                    Toast.makeText(getApplicationContext(), "Bienvenido "+ username, Toast.LENGTH_LONG).show();
+                    Log.d(TAG,"Bienvenido " + username);
 
                     Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
                     intent.putExtra("ACCOUNT", accountEntityString);
-
                     startActivity(intent);
-
                 }else{
-                    Toast.makeText(getApplicationContext(),"Error en la autenticacion",Toast.LENGTH_SHORT).show();
-                    Log.d(TAG,"Error en la autenticacion");
+                    Toast.makeText(getApplicationContext(), "Cuenta no ecnontrada", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "Cuenta no encontrada");
                 }
             }
         });
@@ -60,6 +71,31 @@ public class LoginActivity extends AppCompatActivity {
 
         });
     }
+
+    //Verficar las credenciales en el archivo de cuentas
+    private boolean checkCredentials(String username, String password){
+        try {
+                BufferedReader reader = new BufferedReader(new FileReader(accountsFile));
+                String line;
+                Gson gson = new Gson();
+                while ((line = reader.readLine()) != null){
+                AccountEntity account = gson.fromJson(line, AccountEntity.class);
+                if (account.getUsername().equals(username) && account.getPassword().equals(password)){
+                    accountEntityString = line;
+                    return true;
+                }
+            }
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    //Manejar el resultado de AccountActivity
     ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
@@ -71,6 +107,9 @@ public class LoginActivity extends AppCompatActivity {
                         accountEntityString = data.getStringExtra(AccountActivity.ACCOUNT_RECORD);
                         Gson gson = new Gson();
                         accountEntity =  gson.fromJson(accountEntityString, AccountEntity.class);
+
+                        //Guardar la cuenta en el archivo
+                        saveAccountToFile(accountEntityString);
 
                         String firstname = accountEntity.getFirstname();
 
@@ -85,4 +124,15 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
     );
+
+    private void saveAccountToFile(String accountJson) {
+        try {
+            FileWriter writer = new FileWriter(accountsFile, true);
+            writer.write(accountJson + "\n");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
